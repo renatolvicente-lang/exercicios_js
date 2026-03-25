c803edbf76df8d8d48179911a28b866345041ee7
renatolvicente-lang <renato_l_vicente@estudante.sesisenai.org.br>
2026-03-25 11:51:49 -0300

    feat - continuação da funcionalidade do ex 07

--- scripts/ex05.js
+++ scripts/ex05.js
@@ -1,12 +1,12 @@
 function par_impar(){
     let num = parseInt(prompt("Digite um numero: "));
     let num_par = num % 2;
-    let par;
+    
     if(num_par == 0){
-        par = num + 1;
+        num + 1;
     }else{  
-        par = num + 1;
+        num + 1;
     }
 
-    alert(par);
+    alert(num);
 }
--- scripts/ex07.js
+++ scripts/ex07.js
@@ -1,3 +1,4 @@
 function picoleteria(){
-    alert("Função em desenvolvimento, aguarde...");
+    let escolha = prompt("Sabores Disponives: \n código      Sabor Preço\n a     Chocolate R$1,50\n b     Morango R$2,50\n c    Creme R$2,50\n d    Manga R$ 3,20\n e    Melancia R$3,40\n f   Vanilla ice R$3,00\n g    Céu Azul R$ 3,60\n h   Brownie R$4,00\n i   Hawaiano R$5,00 \nDigite o sabor escolhido da tabela anterior: ");
+    
 }
