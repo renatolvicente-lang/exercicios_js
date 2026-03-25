2b6ab0a78833315eca05e59a03fc9ed96b5c27ba
renatolvicente-lang <renato_l_vicente@estudante.sesisenai.org.br>
2026-03-25 12:29:10 -0300

    feat - finalização do ex07

--- scripts/ex05.js
+++ scripts/ex05.js
@@ -1,12 +1,14 @@
 function par_impar(){
     let num = parseInt(prompt("Digite um numero: "));
     let num_par = num % 2;
-    
+    let par;
     if(num_par == 0){
-        num + 1;
+       num += 1;
+       alert(num);
     }else{  
-        num + 1;
+       num += 1;
+       alert(num);
     }
 
-    alert(num);
+    
 }
--- scripts/ex07.js
+++ scripts/ex07.js
@@ -1,4 +1,37 @@
 function picoleteria(){
-    let escolha = prompt("Sabores Disponives: \n código      Sabor Preço\n a     Chocolate R$1,50\n b     Morango R$2,50\n c    Creme R$2,50\n d    Manga R$ 3,20\n e    Melancia R$3,40\n f   Vanilla ice R$3,00\n g    Céu Azul R$ 3,60\n h   Brownie R$4,00\n i   Hawaiano R$5,00 \nDigite o sabor escolhido da tabela anterior: ");
-    
+    alert("Sabores Disponives: \n código      Sabor Preço\n a     Chocolate R$1,50\n b     Morango R$2,50\n c    Creme R$2,50\n d    Manga R$ 3,20\n e    Melancia R$3,40\n f   Vanilla ice R$3,00\n g    Céu Azul R$ 3,60\n h   Brownie R$4,00\n i   Hawaiano R$5,00 ");
+    let escolha = prompt("Digite o sabor escolhido da tabela anterior: ");
+
+
+
+    switch(escolha){
+        case "a":
+            alert("Seu pedido ficou R$1,50");
+            console.log("Seu pedido ficou R$1,50");
+            break;
+        case "b" :
+            alert("Seu pedido ficou em R$2,50");
+            break;
+        case "c":
+            alert("Seu pedido ficou em R$2,50");
+            break;
+        case "d":            
+            alert("Seu pedido ficou em R$3,20");
+            break;
+        case "e":
+            alert("Seu pedido ficou em R$3,40");
+            break;
+        case "f":
+            alert("Seu pedido ficou em R$3,00");
+            break;
+        case "g":
+            alert("Seu pedido ficou em R$3,60");
+            break;
+        case "h":
+            alert("Seu pedido ficou em R$4,00");
+            break;
+        case "i":
+            alert("Seu pedido ficou em R$5,00");
+            break;
+    }
 }
