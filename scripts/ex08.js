1c6ccee53e867b397b3dbab74b1fc57a714fe25a
Renato L. Vicente <renato_l_vicente@estudante.sesisenai.org.br>
2026-05-12 20:24:59 +0000

    feat - alterações na funcionalidade 8

--- scripts/ex08.js
+++ scripts/ex08.js
@@ -1,21 +1,16 @@
 function op_duasvar(){
-    alert("Funcionalidade em desenvolvimento...");
 
-    let var1 = 1;
-    let var2 = prompt("Digite um numero inteiro: ");
+    let var1 = prompt("Digite um numero inteiro: ");
+    let var2 = prompt("Digite mais um numero inteiro: ");
     let dif;
     let db_tp;
     let mult;
 
-    if (var1 > var2){
-        dif = var1 - var2;
-    }else{
-        dif = var2 - var1
-    }
-
+   
+    dif = var1 - var2;
     db_tp = (var1 * 2) + (var2*  3);
     mult = var1 * var2;
 
-    alert("A diferença entre os números é de: ${dif}/n O Dobro do primeiro numero mais o triplo do segundo número é: ${db_tp}/n A multiplicação dos dois número é de: ${multi}.");
-    
+    alert("A diferença entre os números é de:"+ dif +"\n O Dobro do primeiro numero mais o triplo do segundo número é: "+ db_tp +"\n A multiplicação dos dois número é de: "+ mult +".");
+
 }
